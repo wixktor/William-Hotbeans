f82afd5c024a51d91349846f6d3780c9fe1a7062
wixktor <wiktorstalka05@gmail.com>
2022-06-13 17:06:36 +0100

    main

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// fetch weather from API
+
 function weatherBallon() {
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=london,england&APPID=8400cd76acb7e1e4958b70064d3cf2bd"
@@ -13,6 +15,8 @@ function weatherBallon() {
     });
 }
 
+// show weather from api
+
 function drawWeather(d) {
   var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
 
@@ -22,6 +26,63 @@ function drawWeather(d) {
 window.onload = function () {
   weatherBallon(6167865);
 };
+
+// animate image carousel
+
+var animation = { duration: 25000, easing: (t) => t };
+
+var slider = new KeenSlider("#my-keen-slider", {
+  loop: true,
+  renderMode: "performance",
+  drag: true,
+  breakpoints: {
+    "(min-width: 400px)": {
+      slides: {
+        perView: 1,
+        spacing: 222,
+      },
+    },
+    "(min-width: 1000px)": {
+      slides: {
+        perView: 2,
+        spacing: 222,
+      },
+    },
+  },
+  slides: {
+    perView: 1,
+    spacing: 222,
+  },
+  created(s) {
+    s.moveToIdx(5, true, animation);
+  },
+  updated(s) {
+    s.moveToIdx(s.track.details.abs + 5, true, animation);
+  },
+  animationEnded(s) {
+    s.moveToIdx(s.track.details.abs + 5, true, animation);
+  },
+});
+
+// change background colour
+
+$(document).ready(function () {
+  var scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 3800) {
+      $("body").css("background-color", "white");
+      $(".join").css("color", "#1a1a1a");
+      $(".join-svg").css("fill", "#1a1a1a");
+    } else {
+      $("body").css("background-color", "#1a1a1a");
+      $(".join").css("color", "white");
+      $(".join-svg").css("fill", "white");
+    }
+  });
+});
+
+// change background colour on mobile
 
 $(window).resize(function () {
   if ($(window).width() < 500) {
@@ -41,20 +102,4 @@ $(window).resize(function () {
       });
     });
   }
-});
-
-$(document).ready(function () {
-  var scroll_pos = 0;
-  $(document).scroll(function () {
-    scroll_pos = $(this).scrollTop();
-    if (scroll_pos > 3800) {
-      $("body").css("background-color", "white");
-      $(".join").css("color", "#1a1a1a");
-      $(".join-svg").css("fill", "#1a1a1a");
-    } else {
-      $("body").css("background-color", "#1a1a1a");
-      $(".join").css("color", "white");
-      $(".join-svg").css("fill", "white");
-    }
-  });
 });
